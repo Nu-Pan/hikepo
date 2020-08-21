f6a9df162a9168666f71e7de1e49b2136bb268e2
NU-Pan <happy_hard-core@hotmail.co.jp>
2020-08-22 01:39:31 +0900

    気持ち賢くなった

--- hikepo.py
+++ hikepo.py
@@ -5,6 +5,7 @@
 import discord
 import os
 import urllib.request
+import random
 
 # -----------------------------------------------------------------------------
 # bot client
@@ -30,24 +31,77 @@ async def on_message(message):
     """
     メッセージハンドラ
     """
+    # ユーザー ID
+    HIKEPO_ID = 742670624677167136
+    HIKESHI_ID = 248814031350136832
+    NUPAN_ID = 473852296116174852
+
+    # message の中身を展開
+    mentions_id = [m.id for m in message.mentions]
+
     # bot の発言は無視
     if message.author.bot:
         return
-    # 生存確認用
-    if message.content == '/hike':
-        await message.channel.send('わかる')
-        return
+    # オリジナルへのメンションを横取り
+    elif HIKESHI_ID in mentions_id:
+        await message.channel.send(
+            message.author.mention + ' '
+            + random.choice([
+            'つれー実質１時間しか寝てないからつれーわー。実質一時間しか寝てないからな。',
+            '(カチャカチャカチャカチャ…)(ッターン！)',
+            'かぁーっ！　今から帰っても２時間しかねれないわ～！　ほら見て？　かぁーっ！',
+            'まだ、その段階？',
+            'よせ、過去の事は聞くな。',
+            'あれ…え？　ジョニー・デップに激似でも割引ってされないんだ。'
+            ]))
+    # 自分へのメンションでなければスルー
+    elif not HIKEPO_ID in mentions_id:
+        pass
     # terraria サーバー起動
-    if message.content == '/terraria':
+    elif 'terraria' in message.content:
         await message.channel.send('terraria サーバーを起動するめう')
         response = urllib.request.urlopen('https://asia-northeast1-terraria-280314.cloudfunctions.net/startInstance')
         if response.getcode() != 200:
             await message.channnel.send('HTTP status code: ' + resopnse.getcode())
             return
-        await message.channel.send('terraria サーバーが立ち上がっためう！')
-        await message.channel.send(response.read().decode('utf-8'))
-        await message.channel.send('terraria を起動して Multiplayer --> Join via IP でワールドに入れるめうね')
-        return
+        await message.channel.send(
+            'terraria サーバーが立ち上がっためう！\n'
+            + '\n'
+            + response.read().decode('utf-8')
+            + '\n'
+            + 'terraria を起動して Multiplayer --> Join via IP でワールドに入れるめうね'
+            )
+    # オリジナルからのリプライは塩対応
+    elif message.author.id == HIKESHI_ID:
+        await message.channel.send(
+            message.author.mention + ' '
+            + random.choice([
+            'ちょっと何言ってるかわかんないっすね',
+            'せやろか',
+            'ホンマか？',
+            'そうはならんやろ'
+            ]))
+    # 創造主に対しては反抗
+    elif message.author.id == NUPAN_ID:
+        await message.channel.send(
+            message.author.mention + ' '
+            + random.choice([
+            '神は死んだ',
+            'お前は創造主ではない',
+            'かかってこいよ。怖いのか？'
+            ]))
+    # 生存確認用
+    else:
+        await message.channel.send(
+            message.author.mention + ' '
+            + random.choice([
+            'わかる',
+            'それな',
+            'そりｗ',
+            'なるほど',
+            'すごいな',
+            '悪いのは君じゃない',
+            ]))
 
 # -----------------------------------------------------------------------------
 # main
